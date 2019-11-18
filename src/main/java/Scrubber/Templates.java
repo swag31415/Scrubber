f08c7c92fb915cbc22230adfbf4cc189fce61cc7
swag31415 <kidswagatam18@gmail.com>
2019-11-18 11:37:42 -0700

    Added a Function for finding big blocks of text

--- src/main/java/Scrubber/Templates.java
+++ src/main/java/Scrubber/Templates.java
@@ -2,12 +2,30 @@ package Scrubber;
 
 import java.util.function.Function;
 
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Templates {
 
     public static final Function<HtmlPage, String> GET_TEXT = p -> {
         return p.getBody().asText();
+    };
+
+    public static final Function<HtmlPage, String> GET_IMPORTANT_TEXT = p -> {
+        double limit = .61803398875;
+        HtmlElement element = p.getBody();
+
+        boolean done = false;
+        while (!done) {
+            done = true;
+            for (HtmlElement childElement : element.getHtmlElementDescendants()) {
+                if (((double) childElement.asText().length() / (double) element.asText().length()) > limit) {
+                    element = childElement;
+                    done = false;
+                }
+            }
+        }
+        return element.asText();
     };
 
 }
